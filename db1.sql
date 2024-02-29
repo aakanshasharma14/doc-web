adedcbc45cac39b31c851003e46f3f550e849b28
aakanshasharma14 <140961272+aakanshasharma14@users.noreply.github.com>
2024-02-29 20:14:07 +0530

    Add files via upload

---
+++ db1.sql
@@ -0,0 +1,32 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.04 
+MySQL - 5.0.37-community-nt : Database - db1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db1`;
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `Username` varchar(40) NOT NULL,
+  `Password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+insert  into `login`(`Username`,`Password`) values ('aayush ','1234'),('Aakansha','12345'),('devesh','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
